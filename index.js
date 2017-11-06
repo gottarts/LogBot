38eee03a01be28a614713aea568067771d22a521
matteogottard@me.com <matteogottard@me.com>
2017-11-06 11:27:05 +0100

    first

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.get('/', function (req, res) {
+    console.log(req);
+  res.send('Giao Baolo!');
+});
+
+app.post('/', function (req, res) {
+    console.log(req.body);
+  res.send('Ok');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
