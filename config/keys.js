07c0801075512d249c6e03cb48c78303fe4048ac
matteogottard@me.com <matteogottard@me.com>
2017-11-07 12:18:52 +0100

    fix mlab user e pass

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoUri: 'mongodb://<dbuser>:<dbpassword>@ds149855.mlab.com:49855/heroku_wctfdnpz'
+    mongoUri: 'mongodb://barone:eutz257@ds149855.mlab.com:49855/heroku_wctfdnpz'
 };
